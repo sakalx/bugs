ac5c92ba9de166fde3a35f23373edfaf11bea0b0
sakal <sakals@mail.ua>
2019-05-10 22:12:22 -0400

    feat: Animation on hover cube side.
    Signed-off-by: sakal <sakals@mail.ua>

--- src/components/Cube/Side/style.js
+++ src/components/Cube/Side/style.js
@@ -3,11 +3,18 @@ import {fadeIn} from 'root/theme/keyframes';
 
 export const Wrap = styled('div')`
   align-items: center;
+  background-color: white;
+  box-shadow: 0 3px 5px -1px rgba(0, 0, 0, 0.2), 0px 5px 8px 0px rgba(0, 0, 0, 0.14), 0px 1px 14px 0px rgba(0, 0, 0, 0.12);
   display: flex;
   flex-direction: column;
   height: 100%;
   justify-content: center;
+  transition: transform .3s cubic-bezier(.57,.21,.69,1.25);
   user-select: none;
+  
+  :hover {
+    transform: rotate(360deg);
+  }
 `;
 
 export const Title = styled('h3')`
