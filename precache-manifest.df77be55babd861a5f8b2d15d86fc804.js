99d78850cf2b7ff55ea0d31fb3a36daa0addf100
sakal <sakals@mail.ua>
2019-06-04 18:43:43 -0400

    Updates

---
+++ precache-manifest.df77be55babd861a5f8b2d15d86fc804.js
@@ -0,0 +1,73 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "url": "icons/android/icon_144x144.aa677224b062dce1593ac7184d93d8ca.png"
+  },
+  {
+    "url": "icons/android/icon_192x192.b2fb8212a3e693c40b53fc304fb359bd.png"
+  },
+  {
+    "url": "icons/android/icon_36x36.2ead588749342ba8b896f6f7455ab273.png"
+  },
+  {
+    "url": "icons/android/icon_48x48.1e86019b2fe9abe4eed42c3357c9ab70.png"
+  },
+  {
+    "url": "icons/android/icon_512x512.c9eb193fe937dbe80bc3e5175c0cfadf.png"
+  },
+  {
+    "url": "icons/android/icon_72x72.8d706b6d613dc37262e9954a85d21e1f.png"
+  },
+  {
+    "url": "icons/android/icon_96x96.2fab366bc9bb82b86253fd0fb2bcdea3.png"
+  },
+  {
+    "url": "icons/ios/icon_1024x1024.812dccaaee4bff50165539231258223d.png"
+  },
+  {
+    "url": "icons/ios/icon_120x120.c5e2bea16cd70cdd852a87ae532283b4.png"
+  },
+  {
+    "url": "icons/ios/icon_152x152.813a2b05f2067319126ace9d1778718e.png"
+  },
+  {
+    "url": "icons/ios/icon_167x167.97d9502be2db612ba76f4098c98becc2.png"
+  },
+  {
+    "url": "icons/ios/icon_180x180.dfdf66b8394b5c83d9d86d0a741d25ea.png"
+  },
+  {
+    "revision": "33a28f10b3d8e03f7fbb5db4773aa15e",
+    "url": "index.html"
+  },
+  {
+    "revision": "1e96d8edcdc97518cc0c",
+    "url": "js/1.bundle.js"
+  },
+  {
+    "revision": "62b6b5d6454b7a13cafb",
+    "url": "js/2.bundle.js"
+  },
+  {
+    "revision": "8bf50f17450ad7e3be7a",
+    "url": "js/3.bundle.js"
+  },
+  {
+    "revision": "c340c69912572f4bbaa6",
+    "url": "js/4.bundle.js"
+  },
+  {
+    "revision": "7d4c2693afe84272410e",
+    "url": "js/5.bundle.js"
+  },
+  {
+    "revision": "c435cf52912d2bf88192",
+    "url": "js/6.bundle.js"
+  },
+  {
+    "revision": "ce9fe55b54da018c2473",
+    "url": "js/main.bundle.js"
+  },
+  {
+    "url": "manifest.d473485cbb3d72fb9bd597f178bc28a2.json"
+  }
+]);
